fbab5ac532304baae87f5e5cf982f5088c9af5a0
Shivashankarmasuldar <shivashankarsm97@gmail.com>
2023-04-07 09:17:14 +0530

    Intial Draft

---
+++ DemoProject/src/test/java/com/sgtesting/tests/Users.java
@@ -0,0 +1,23 @@
+package com.sgtesting.tests;
+
+import org.testng.annotations.Test;
+
+public class Users {
+
+	@Test
+	public void method1()
+	{
+		System.out.println("Its a Mathod1 Statement");
+	}
+	
+	@Test
+	public void method2()
+	{
+		System.out.println("Its a Mathod2 Statement");
+	}
+	@Test
+	public void method3()
+	{
+		System.out.println("Its a Mathod3 Statement");
+	}
+}
